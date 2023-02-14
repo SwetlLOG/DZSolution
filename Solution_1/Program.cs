b19e43b465b3a2a640feb4490d227f0af3588a7d
SwetlLOG <sweetlog@mail.ru>
2023-02-14 14:13:11 +0300

    Solution_1/

---
+++ Solution_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// По двум заданным числам проверить является ли первое
+// квадратом второго
+
+int a = 25;
+int b = 5;
+
+if (b*b==a)
+{
+  Console.WriteLine("Yes");
+}
+else
+{
+  Console.WriteLine("No");
+}

075532f94393c12647299a9333a6b9965d385067
SwetlLOG <sweetlog@mail.ru>
2023-02-14 18:14:37 +0300

    Solution_3/

---
+++ Solution_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("Введите число от 1 до 7 ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (n == 1)
+{
+  Console.WriteLine ("Monday");
+}
+if (n == 2)
+{
+  Console.WriteLine ("Tuesday");
+}
+if (n == 3)
+{
+  Console.WriteLine ("Wednesday");
+}
+if (n == 4)
+{
+  Console.WriteLine ("Thursday");
+}
+if (n == 5)
+{
+  Console.WriteLine ("Friday");
+}
+if (n == 6)
+{
+  Console.WriteLine ("Saturday");
+}
+if (n == 7)
+{
+  Console.WriteLine ("Sunday");
+}

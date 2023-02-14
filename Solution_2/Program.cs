2e3eceee842e2c89b1512cd87d69fb7a43faee3a
SwetlLOG <sweetlog@mail.ru>
2023-02-14 14:55:38 +0300

    Solution_2/

---
+++ Solution_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Даны два числа. Показать большее и меньшее.
+int a=8;
+int b=6;
+int max;
+int min;
+if (a>b)
+{
+  max=a; min=b;
+}
+else
+{
+  max=b; min=a;
+}
+Console.Write("max = ");
+Console.WriteLine( max );
+Console.Write("min = ");
+Console.WriteLine( min );

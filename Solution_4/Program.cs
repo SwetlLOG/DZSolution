f430fff2606e420dbe7c957daa5ca04810d48125
SwetlLOG <sweetlog@mail.ru>
2023-02-14 17:50:24 +0300

    Solution_4/

---
+++ Solution_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Write("Введите первое число  ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число  ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число  ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+
+int max =a;
+
+if (b>a) max = b;
+if (c>b) max = c;
+
+Console.WriteLine ("max = ");
+Console.Write (max );
